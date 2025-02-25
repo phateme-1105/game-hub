b4cf7ff3a58387d772809eb5da23a243c94e8000
Fatemeh sanjari <fatemehsanjari5178@gmail.com>
2025-02-25 18:45:54 +0330

    Refactor:remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,4 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import React from "react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
@@ -11,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" border={10} overflow={"hidden"}>
+    <Card border={10} overflow={"hidden"}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+}
+const GameCardContainer = ({ children }: Props) => {
+  return <Box width="300px">{children}</Box>;
+};
+
+export default GameCardContainer;
