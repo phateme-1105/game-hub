2318bfbeb4203e5c6884dccdc56331783fbd5bf1
Fatemeh sanjari <fatemehsanjari5178@gmail.com>
2025-02-28 20:41:29 +0330

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -13,7 +13,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode </Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
