2d703dc96c1d8df623a72fcd2790e1428c6d8698
Fatemeh sanjari <fatemehsanjari5178@gmail.com>
2025-03-02 10:50:21 +0330

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,6 @@
 
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -15,6 +14,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
